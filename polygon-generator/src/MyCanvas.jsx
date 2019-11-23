2ea2a1c9c9e8c3fa9314dd707e9569d2a91746ba
Samuel <sheldak426@gmail.com>
2019-11-23 16:33:15 +0100

    added relative click points position

--- polygon-generator/src/MyCanvas.jsx
+++ polygon-generator/src/MyCanvas.jsx
@@ -1,27 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyCanvas extends Component {
-    state = {
-        points: [],
-        lines: []
-    }
-    
-    onCanvasClick = (event) => {
-        // event.screenX =
-        console.log("item clicked at coord " + event.clientX + " " + event.clientY)
+  state = {
+    points: [],
+    lines: []
+  };
+
+  onCanvasClick = event => {
+    // event.screenX =
+    console.log("item clicked at coord " + event.clientX + " " + event.clientY);
+    const style = getComputedStyle(document.querySelector("#canvasCont"));
+    const point = [event.clientX, event.clientY];
+    const changes = [
+      parseInt(
+        style.left
+          .split("")
+          .slice(0, -2)
+          .join("")
+      ),
+      parseInt(
+        style.top
+          .split("")
+          .slice(0, -2)
+          .join("")
+      )
+    ];
+    const newPoint = [point[0] - changes[0], point[1] - changes[1]];
+    console.log("item clicked at coord " + newPoint[0] + " " + newPoint[1]);
     //     const points = [...this.state.points];
     // const index = counters.indexOf(counter);
     // counters[index] = { ...counter };
     // counters[index].value++;
     // this.setState({ counters })
+  };
 
-    }
-
-    state = {}
-    render() {
-        return (<div id="canvas" onClick={this.onCanvasClick}></div>);
-    }
+  state = {};
+  render() {
+    return <div id="canvas" onClick={this.onCanvasClick}></div>;
+  }
 }
 
 export default MyCanvas;
-
