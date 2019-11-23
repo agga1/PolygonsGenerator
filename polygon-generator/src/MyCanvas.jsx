e714d7d1280b831c36aef1b04ece7e1482e23b58
agga1 <agud100@gmail.com>
2019-11-23 16:40:38 +0100

    insertion mode saved

--- polygon-generator/src/MyCanvas.jsx
+++ polygon-generator/src/MyCanvas.jsx
@@ -1,23 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyCanvas extends Component {
-    state = {
-        points: [],
-        lines: []
-    }
+  state = {
+    points: [],
+    lines: [],
+    mode: "points"
+  };
 
-    onCanvasClick = (event) => {
-        console.log("item clicked at coord " + event.clientX + " " + event.clientY)
-        console.log(" state ", this.state.points)
+  onCanvasClick = event => {
+    console.log(
+      this.state.mode +
+        " clicked at coord " +
+        event.clientX +
+        " " +
+        event.clientY
+    );
+    switch (this.state.mode) {
+      case "points":
+        console.log(" state ", this.state.points);
         const newPoint = [event.clientX, event.clientY];
         const points = [...this.state.points, newPoint];
-        this.setState({ points })
+        this.setState({ points });
+        break;
     }
-
-    render() {
-        return (<div id="canvas" onClick={this.onCanvasClick}></div>);
-    }
+  };
+  onChangeMode = mode => {
+    console.log("new mode " + mode);
+    this.setState({ mode });
+  };
+  render() {
+    return (
+      <>
+        <div id="canvas" onClick={this.onCanvasClick}></div>
+        <button id="enter-points" onClick={() => this.onChangeMode("points")}>
+          Enter points
+        </button>
+        <button id="enter-lines" onClick={() => this.onChangeMode("lines")}>
+          Enter lines
+        </button>
+      </>
+    );
+  }
 }
 
 export default MyCanvas;
-
